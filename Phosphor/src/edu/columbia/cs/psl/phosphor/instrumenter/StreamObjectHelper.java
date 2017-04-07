0fe4cd48d7e0fbe59f420b6d0968357189b2ddcf
jianyu <jianyu@hku.hk>
2017-04-07 20:16:43 +0800

    Stream Object[FINISH]
    A Stream Object helper is added to fix the problem of Array[_]
    in scala

---
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/StreamObjectHelper.java
@@ -0,0 +1,8 @@
+package edu.columbia.cs.psl.phosphor.runtime;
+
+/**
+ * Created by jianyu on 4/7/17.
+ */
+public class StreamObjectHelper {
+
+}
---
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/StreamObjectMV.java
@@ -0,0 +1,36 @@
+package edu.columbia.cs.psl.phosphor.runtime;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+
+/**
+ * Created by jianyu on 4/7/17.
+ */
+public class StreamObjectMV extends MethodVisitor {
+    StreamObjectMV(MethodVisitor mv) {
+        super(Opcodes.ASM5, mv);
+    }
+
+    @Override
+    public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itfc) {
+        if (owner.equals("sun/misc/Unsafe") && name.equals("putObjectWrapper")) {
+            System.out.println(desc);
+            desc = "(Lsun/misc/Unsafe;Ljava/lang/Object;JLjava/lang/Object;Ljava/lang/Class;)V";
+            super.visitMethodInsn(Opcodes.INVOKESTATIC,
+                    Type.getInternalName(StreamObjectHelper.class),
+                    name, desc, itfc);
+            System.out.println("found");
+            return;
+        } else if (owner.equals("sun/misc/Unsafe") && name.equals("putObjectWrapper$$PHOSPHORTAGGED")) {
+            System.out.println(desc);
+            desc = "(Lsun/misc/Unsafe;Ljava/lang/Object;IJLjava/lang/Object;Ljava/lang/Class;)V";
+            super.visitMethodInsn(Opcodes.INVOKESTATIC,
+                    Type.getInternalName(StreamObjectHelper.class),
+                    name, desc, itfc);
+            System.out.println("found-tag");
+            return;
+        }
+        super.visitMethodInsn(opcode, owner, name, desc, itfc);
+    }
+}
